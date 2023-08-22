3d6295e91c4fef2c276115cd7626962dfca9188a
russell258 <duckie258@gmail.com>
2023-08-22 23:02:24 +0800

    set up

---
+++ src/test/java/sg/edu/nus/iss/csfday36_backend/Csfday36BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.csfday36_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csfday36BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
